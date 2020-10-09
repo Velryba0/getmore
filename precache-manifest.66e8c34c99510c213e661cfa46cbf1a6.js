5518f8a8bc7fc8f262a37a7b3b8076f85e2ecae7
Nandu <fernando.sabaiz@gmail.com>
2020-10-09 18:32:59 -0500

    Updates

---
+++ precache-manifest.66e8c34c99510c213e661cfa46cbf1a6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "200fc53fd3baf5179dbce887c546e242",
+    "url": "/Velryba0/getmore/index.html"
+  },
+  {
+    "revision": "0a63f24b45e8a1b1017b",
+    "url": "/Velryba0/getmore/static/css/main.2034ce9e.chunk.css"
+  },
+  {
+    "revision": "52855293c65df8bc94cf",
+    "url": "/Velryba0/getmore/static/js/2.4e929a29.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Velryba0/getmore/static/js/2.4e929a29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a63f24b45e8a1b1017b",
+    "url": "/Velryba0/getmore/static/js/main.a26f4b5a.chunk.js"
+  },
+  {
+    "revision": "4c8518f05478a99e9823",
+    "url": "/Velryba0/getmore/static/js/runtime-main.357229c9.js"
+  },
+  {
+    "revision": "7f281199258d96e249a7fce4101006b9",
+    "url": "/Velryba0/getmore/static/media/Helvetica Neu Bold.7f281199.ttf"
+  },
+  {
+    "revision": "bb5671edae4b3cecbd3c98159511f2ea",
+    "url": "/Velryba0/getmore/static/media/HelveticaNeueLt.bb5671ed.ttf"
+  },
+  {
+    "revision": "ce546fef19234a3a3990cda0f7cdd235",
+    "url": "/Velryba0/getmore/static/media/bg-section-four.ce546fef.png"
+  },
+  {
+    "revision": "27c3d0b969d7b7ac019ac3e4822cf38d",
+    "url": "/Velryba0/getmore/static/media/carousel-controls.27c3d0b9.png"
+  },
+  {
+    "revision": "dade1fd8e136dcb28e819d3d1e35220a",
+    "url": "/Velryba0/getmore/static/media/group.dade1fd8.png"
+  },
+  {
+    "revision": "5f5a5c14df7549aab218c080d0e49b72",
+    "url": "/Velryba0/getmore/static/media/racket.5f5a5c14.png"
+  }
+]);
